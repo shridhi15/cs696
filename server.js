4f0451405719ebae106b5196c14d6b9452ca7e4b
Shridhi Patel <patelshridhi15@gmail.com>
2026-04-12 02:21:59 -0400

    Trigger GA via JS comment

--- server.js
+++ server.js
@@ -41,4 +41,4 @@ const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on :${PORT}`));
 module.exports = app;
 console.log("CORRECT SERVER USED");
-// Lab 7 trigger - Correct Server Used
+// Lab 7 trigger
